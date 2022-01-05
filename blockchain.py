19c889dc40ff80ad86b355d3d91635aa9cb772b3
VitoTassielli <vitotassielli@gmail.com>
2022-01-05 19:52:14 +0100

    Add create_block() function

--- blockchain.py
+++ blockchain.py
@@ -9,3 +9,14 @@ class Blockchain:
     def __init__(self):                                        # self is the instance of the class (it's used like "this" in C++)
         self.chain = []                                        # inizializing the chain (as list)
         self.create_block(proof = 1, previous_hash = '0')      # inizializing the Genesis Block of the Blockchain
+
+
+    # create_block() function return a block passed the proof (found by the miner) and the previous_hash (to link the new block at the blockchain)
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash}
+
+        self.chain.append(block)
+        return block   
